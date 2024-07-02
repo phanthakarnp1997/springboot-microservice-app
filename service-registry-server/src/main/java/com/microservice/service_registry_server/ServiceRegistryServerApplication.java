fd8569f819bcfe83c307d5ae75c4e8bb2243e1e8
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2024-07-02 21:31:02 +0700

    initial

---
+++ service-registry-server/src/main/java/com/microservice/service_registry_server/ServiceRegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.service_registry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryServerApplication.class, args);
+	}
+
+}
---
+++ service-registry-server/src/test/java/com/microservice/service_registry_server/ServiceRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.service_registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
